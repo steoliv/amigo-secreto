da1693f309b01d5d00b5202ab86cb474e6c29777
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2025-02-20 19:31:01 -0300

    Função que alimenta o array com nome dos amigos

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos =[];
+
+
+function adicionarAmigo(){
+    let nomeAmigo = document.querySelector('.input-name').value;
+    listaDeAmigos.push(nomeAmigo);
+}
