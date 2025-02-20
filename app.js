37009397494c6faf45f1cbea4e4b8c4039b1e89e
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2025-02-20 19:57:09 -0300

    Função para limpar input após cada inserção de  nome

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 let listaDeAmigos =[];
 
 
+//Função para cadastrar amigos na lista
 function adicionarAmigo(){
     let nomeAmigo = document.querySelector('.input-name').value;
     listaDeAmigos.push(nomeAmigo);
+
+    console.log(listaDeAmigos);
+
+    limparInput();
+}
+
+//Função para limpar input após cada inserção de nome
+function limparInput(){
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
 }
