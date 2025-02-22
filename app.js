dd6e7c95b4e331e70b8ba17d982249c949e3908c
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2025-02-22 00:23:21 -0300

    Incluindo o resultado no front

--- app.js
+++ app.js
@@ -26,11 +26,13 @@ function exibirAmigos(){
 //Função para sortear amigo
 function sortearAmigo(){
     let sorteado = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
+    let exibirSorteado = document.querySelector(".result-list"); 
 
     //Loop para não repetir o nome removendo-o da lista
     for(i=0; i <= listaDeAmigos.length; i++){
         if(listaDeAmigos.includes(sorteado)){
             listaDeAmigos.splice(listaDeAmigos.indexOf(sorteado), 1);
+            exibirSorteado.innerHTML = `O nome do seu amigo secreto é ${sorteado}`;
         }
     }
 
